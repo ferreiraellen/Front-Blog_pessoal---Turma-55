8203c50b7b4d56cf38052ce7a5cdf8ae9f3e6d4e
unknown <ferreira.ellenk@gmail.com>
2022-09-05 11:46:37 -0300

    Configuração cadastro e login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Userlogin } from '../model/Userlogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userlogin: Userlogin = new Userlogin
 
-  ngOnInit(): void {
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit()  {
+    window.scroll(0,0)
   }
 
+login (){
+  this.auth.login(this.userlogin).subscribe((resp: Userlogin) =>{
+    this.userlogin = resp
+
+
+    environment.token = this.userlogin.token
+    environment.nome = this.userlogin.nome
+    environment.foto = this.userlogin.foto
+    environment.id = this.userlogin.id
+
+    
+
+    this.router.navigate(['/inicio'])
+  }, erro =>{
+    if(erro.status == 500 || erro.status == 400){
+      alert('Usuário ou senha está incorreto!')
+  }
+  })
+}
 }
