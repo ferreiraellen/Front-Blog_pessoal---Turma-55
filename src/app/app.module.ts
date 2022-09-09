39ab9be74c03e56f82a5cd8bd6a853991fb52d87
unknown <ferreira.ellenk@gmail.com>
2022-09-08 23:27:18 -0300

    Incusão de put e delete Postagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delte/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { PostagemDeleteComponent } from './delte/postagem-delete/postagem-delete.component';
 
 
 @NgModule({
@@ -26,7 +28,9 @@ import { TemaDeleteComponent } from './delte/tema-delete/tema-delete.component';
     InicioComponent,
     TemaComponent,
     TemaEditComponent,
-    TemaDeleteComponent
+    TemaDeleteComponent,
+    PostagemEditComponent,
+    PostagemDeleteComponent
   ],
   imports: [
     BrowserModule,
